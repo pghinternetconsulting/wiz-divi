2d20c978cb6b2176ae8e13e31d707f50899883ca
Ramiro Iriñiz <mrlomein@gmail.com>
2022-01-07 11:46:15 -0300

    Implemented Search Functionalities

--- themes/Divi-Child/functions.php
+++ themes/Divi-Child/functions.php
@@ -17,4 +17,31 @@ function use_gd_editor($array) {
 	return array( 'WP_Image_Editor_GD', ); 
 }
 add_filter( 'wp_image_editors', 'use_gd_editor' );
+
+/* Search */	
+function wpbsearchform( $form ) {
+   
+    $form = '<form role="search" class="search-container" method="get" autocomplete="off" id="searchform" action="' . home_url( '/' ) . '" >
+    <input  id="search-box" type="text" value="' . get_search_query() . '" class="search-box" name="s" />
+    <label for="search-box"><span class="et-pb-icon search-icon">&#x55;</span></label>
+    <input type="submit" id="search-submit" />
+    </form>';
+   
+    return $form;
+}
+   
+add_shortcode('wpbsearch', 'wpbsearchform');
+
+function wpbsearchform_mob( $form ) {
+   
+    $form = '<form role="search" method="get" autocomplete="off" id="searchform" action="' . home_url( '/' ) . '" >
+    <input type="text" value="' . get_search_query() . '" name="s" id="s">
+    <input type="submit" id="searchsubmit" value="Search">
+    </form>';
+   
+    return $form;
+}
+   
+add_shortcode('wpbsearch_mob', 'wpbsearchform_mob');
+
 ?>
